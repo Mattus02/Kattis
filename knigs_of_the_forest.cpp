8c7dd736d8c90613802adb18e3735fd20a26706b
Mattias <mattias.fhc@gmail.com>
2023-06-13 10:16:35 +0200

    knigs of the forest

---
+++ knigs_of_the_forest.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int pool, years;
+    cin >> pool >> years;
+
+    priority_queue<int> curr;
+    vector<int> others(years);
+
+    int y, str, s;
+    cin >> y >> str;
+
+    if (y == 2011)
+        curr.push(str);
+    others[y-2011] = str;
+
+    for (int i = 0; i < pool+years-2; ++i)
+    {
+        cin >> y >> s;
+        if (y == 2011)
+            curr.push(s);
+        others[y-2011] = s;
+    }
+
+    others[0] = 0;
+    bool found = false;
+    for (int i = 0; i < years; ++i)
+    {
+        curr.push(others[i]);
+        int max = curr.top();
+        curr.pop();
+        if (max == str) {
+            cout << 2011+i;
+            found = true;
+            break;
+        }
+    }
+
+    if (!found)
+        cout << "unknown";
+}
